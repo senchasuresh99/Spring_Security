637d80e73761b48cd62b61b792496ed2ab5ebe2e
Suresh <senchasuresh99@gmail.com>
2023-08-08 16:51:36 +0530

    Commit By Suresh Sencha

---
+++ springsecurity1/src/main/java/com/security/springsecurity1/SpringsecuritybasicApplication.java
@@ -0,0 +1,13 @@
+package com.security.springsecurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+	}
+
+}
