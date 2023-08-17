dd010185d940139c7c8539ad9cc7f7419de0a0bb
Suresh <senchasuresh99@gmail.com>
2023-08-17 13:52:42 +0530

    CSRF (Suresh Sencha)

---
+++ springsecsection6latest/src/main/java/com/eazybytes/controller/LoginController.java
@@ -0,0 +1,63 @@
+package com.eazybytes.controller;
+
+
+import java.security.Principal;
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+
+@RestController
+public class LoginController {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	 @Autowired
+	    private PasswordEncoder passwordEncoder;
+	
+	@PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity<String> response = null;
+        try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+            customer.setCreateDt(String.valueOf(new Date(System.currentTimeMillis())));
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to " + ex.getMessage());
+        }
+        return response;
+    }
+
+	
+	@RequestMapping("/user")
+	public Customer getUserDetailsAfterLogin(Principal user) {
+		List<Customer> customers = customerRepository.findByEmail(user.getName());
+		if (customers.size() > 0) {
+			return customers.get(0);
+		}else {
+			return null;
+		}
+		
+	}
+
+}
