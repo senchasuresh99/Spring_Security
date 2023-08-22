830395d4ec4180a2380fc0982aa8030005735a9a
Suresh <senchasuresh99@gmail.com>
2023-08-22 12:49:11 +0530

    OAUTH2 demo using Github Auth server(Suresh Sencha)

---
+++ springsecOAUTH2GitHub/src/main/java/com/eazybytes/springsecOAUTH2GitHub/config/SpringSecOAUTH2GitHubConfig.java
@@ -0,0 +1,41 @@
+package com.eazybytes.springsecOAUTH2GitHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecOAUTH2GitHubConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests)->requests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+    
+    /*@SuppressWarnings("removal")
+	@Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated().and().oauth2Login();
+        return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRepository() {
+        ClientRegistration clientReg = clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientReg);
+    }
+
+    private ClientRegistration clientRegistration() {
+		return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("67a39ac05ac717e66443")
+	           .clientSecret("3afb46848500beb224834ff72961a6f2e44004d4").build();
+	 }*/
+
+}
